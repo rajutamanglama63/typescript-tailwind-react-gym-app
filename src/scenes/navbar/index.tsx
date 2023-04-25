e2b539190e4f2987c7e4a8335cc94c8cd5fbe1d3
rajutamanglama65 <raju@fusemachines.com>
2023-04-25 12:04:53 +0545

    feat: navbar functionality completed

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -4,17 +4,22 @@ import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
+import { useState } from "react";
 
 type Props = {
+  isTopOfPage: boolean
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void
 };
 
-const Navbar = ({selectedPage, setSelectedPage}: Props) => {
+const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}: Props) => {
   const flexBetween = "flex items-center justify-between";
   const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)")
+  const [isMenuToggled, setMenuToggled] = useState<boolean>(false)
+  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow"
+
   return <nav>
-    <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+    <div className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
 
       <div className={`${flexBetween} mx-auto w-5/6`}>
 
@@ -35,7 +40,7 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
             </div>
           </div>
           ) : (
-            <button className="rounded-full bg-secondary-500 p-2">
+            <button className="rounded-full bg-secondary-500 p-2" onClick={() => setMenuToggled(true)}>
               <Bars3Icon className="h-6 w-6 text-white" />
             </button>
           )}
@@ -46,6 +51,27 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
       </div>
 
     </div>
+
+    {/* MOBILE MENU MODAL */}
+    {!isAboveMediumScreen && isMenuToggled && (
+      <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+        {/* CLOSE ICON */}
+        <div className="flex justify-end p-12">
+          <button onClick={() => setMenuToggled(!isMenuToggled)}>
+            <XMarkIcon className="h-6 w-6 text-grey-400" />
+          </button>
+        </div>
+
+        {/* MENU ITEMS */}
+        <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+          <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+          <Link page="Benifits" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+          <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+          <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+        </div>
+      </div>
+    )}
+
   </nav>;
 };
 
